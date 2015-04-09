16015a883900ec328bc59eb49473dec81fa87a46
Robert Dowling <rob@t430>
2015-04-09 16:18:52 -0700

    Add new test, based on failures seen in cc3200 quickstart.
    Motivated by parsing push notification output from parse.com
    
    New test compiles c99, not c++

---
+++ common/tests/robtest1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "../simplejson.h"
+
+char *data = "{\"data\":{\"alert\":true},\"time\":\"2015-04-07T22:43:00.4Z\"}";
+#define vlen 1024
+char value[vlen];
+
+int main (int c, char **argv)
+{
+	char *key;
+	int x;
+
+	key = "alert";
+	x=simpleJsonProcessor(data, key, value, vlen);
+	printf ("x=%d data=%s key=%s value=%s\n", x, data, key, value);
+
+	key = "time";
+	x=simpleJsonProcessor(data, key, value, vlen);
+	printf ("x=%d data=%s key=%s value=%s\n", x, data, key, value);
+	
+	key = "bloop";
+	x=simpleJsonProcessor(data, key, value, vlen);
+	printf ("x=%d data=%s key=%s value=%s\n", x, data, key, value);
+	
+	key = "true";
+	x=simpleJsonProcessor(data, key, value, vlen);
+	printf ("x=%d data=%s key=%s value=%s\n", x, data, key, value);
+	
+	key = "data";
+	x=simpleJsonProcessor(data, key, value, vlen);
+	printf ("x=%d data=%s key=%s value=%s\n", x, data, key, value);
+	
+	char value2[vlen];
+	key = "alert";
+	x=simpleJsonProcessor(value, key, value2, vlen);
+	printf ("x=%d data=%s key=%s value=%s\n", x, value, key, value2);
+	
+	return 0;
+}
